17b949e5028059dd5d1a8bfbfb8c0104afa0bbb2
anfi1622 <anfi1622@E2404-01-1712>
2018-01-23 12:31:23 +0100

    did some work on views with Jonatan

---
+++ ServerSide-Project/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServerSide_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.test = 1;
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            ViewBag.test = 2;
+            return View("Index");
+        }
+    }
+}
