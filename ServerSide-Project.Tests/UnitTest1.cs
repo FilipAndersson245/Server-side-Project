c5fd7be0f1ea19c86eafa6929c9db9b114e75831
anfi1622 <anfi1622@E2404-01-1712>
2018-01-24 16:12:32 +0100

    worked some on unit tests

--- ServerSide-Project.Tests/UnitTest1.cs
+++ ServerSide-Project.Tests/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerSide_Project.Controllers;
+using System.Web.Mvc;
+using System.Collections.Generic;
 
 namespace ServerSide_Project.Tests
 {
@@ -7,8 +10,15 @@ namespace ServerSide_Project.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ListBooks()
         {
+            var Control = new ListController();
+            var res = Control.ListBooks() as ViewResult;
+            var model = res.Model as IEnumerable<Models.Book>;
+            foreach(var item in model)
+            {
+                Assert.AreEqual("Tolkien", item.Author.LastName);
+            }
         }
     }
 }
