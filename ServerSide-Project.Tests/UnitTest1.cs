17f2ab1ce79b9dfcbbc813d9a48d43f96b3a45f0
Filip Andersson <jabos123@gmail.com>
2018-01-27 20:41:16 +0100

    added MockHttpSession

--- ServerSide-Project.Tests/UnitTest1.cs
+++ ServerSide-Project.Tests/UnitTest1.cs
@@ -8,14 +8,23 @@ using ServerSide_Project.Models;
 
 namespace ServerSide_Project.Tests
 {
+    public class MockHttpSession : HttpSessionStateBase
+    {
+        Dictionary<string, object> _sessionDictonary = new Dictionary<string, object>();
+        public override object this[string name] { get => _sessionDictonary[name]; set => _sessionDictonary[name] = value; }
+    }
+
     [TestClass]
     public class UnitTest1
     {
+
+
+
         [TestMethod]
         public void Home()
         {
             var context = new Mock<ControllerContext>();
-            var session = new Mock<HttpSessionStateBase>();
+            var session = new Mock<MockHttpSession>();
 
             context.Setup(m => m.HttpContext.Session).Returns(session.Object);
 
@@ -27,6 +36,7 @@ namespace ServerSide_Project.Tests
             //controler.Session["repo"] = new Repository();
             var res = controler.Index() as ViewResult;
             Assert.AreEqual("Home", res.ViewName);
+            res.
             
 
             //wip
