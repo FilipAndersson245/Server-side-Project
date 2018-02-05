119ed42161e39e4c8d65ec3b74540d1b5663e24f
Filip Andersson <jabos123@gmail.com>
2018-02-05 22:12:02 +0100

    did a sql querry and renamed repo classes

---
+++ Repository/Support/EBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace Repository.Support
+{
+    public class EBook
+    {
+        public static List<AUTHOR> GetAuthorsFromIsbn(string isbn)
+        {
+            using (var db = new dbGrupp3())
+            {
+                //string isbn = "0070062722";
+                return db.Database.SqlQuery<AUTHOR>(
+                     @"SELECT dbo.AUTHOR.FirstName, dbo.AUTHOR.LastName , dbo.AUTHOR.BirthYear , dbo.AUTHOR.Aid 
+                    FROM ( dbo.AUTHOR INNER JOIN BOOK_AUTHOR ON AUTHOR.Aid = BOOK_AUTHOR.Aid AND BOOK_AUTHOR.ISBN = @isbn)",
+                     new SqlParameter("@isbn", isbn)).ToList();
+
+            }
+        }
+    }
+}
--- ServerSide-Project/Controllers/HomeController.cs
+++ ServerSide-Project/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
 
 namespace ServerSide_Project.Controllers
 {
@@ -16,15 +17,6 @@ namespace ServerSide_Project.Controllers
         {
             ServerSide_Project.Models.Repository repo = new ServerSide_Project.Models.Repository();
             Session["repo"] = repo;
-
-            using (var db = new dbGrupp3())
-            {
-                //db.Database.Connection.Open();
-                foreach (var book in db.BOOKs)
-                {
-                    Console.WriteLine(book.Title);
-                }
-            }
 
             return View("Home");
 
