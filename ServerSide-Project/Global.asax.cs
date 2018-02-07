f54d3853d450ae2cd9a098cc25dfed87d0e2fc64
Filip Andersson <anfi1622@student.ju.se>
2018-02-07 11:26:58 +0100

    set a todo comment

--- ServerSide-Project/Global.asax.cs
+++ ServerSide-Project/Global.asax.cs
@@ -13,6 +13,9 @@ namespace ServerSide_Project
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //todo add
+            //a map   Configuration and class/function
+            //Service.Configuration.AutoMapperConfig.Configure();
         }
     }
 }
