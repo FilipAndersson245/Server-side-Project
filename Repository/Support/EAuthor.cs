3fdca7ea36d96d29dbf2d3411fd40de8c1fff958
jonatan-flyckt <jonatan.flyckt@hotmail.com>
2018-02-08 12:57:34 +0100

    Work on AuthorList and AuthorDetails

--- Repository/Support/EAuthor.cs
+++ Repository/Support/EAuthor.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.Data.SqlClient;
 
 namespace Repository.Support
 {
     public class EAuthor
     {
 
+        public static List<AUTHOR> getAllAuthorsFromDB()
+        {
+            using (var db = new dbGrupp3())
+            {
+                return db.AUTHORs.OrderBy(x => x.LastName).ToList();
+            }
+        }
 
+        public static AUTHOR getAuthorDetailsFromDB(string id)
+        {
+            using (var db = new dbGrupp3())
+            {
+                return db.AUTHORs.Find(id);
+            }
+        }
 
     }
+
 }
