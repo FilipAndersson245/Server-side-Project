17b5c5bc1fdbfd9eb644397d52eb3449da168392
Filip Andersson <anfi1622@student.ju.se>
2018-02-28 14:18:46 +0100

    added the ValidationMessages class that convert codes of erros to strings in a modelstate in the view atm placed tempdata inside it

---
+++ ServerSide-Project/Tools/ValidationMessages.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Web.ModelBinding;
+
+namespace ServerSide_Project.Tools
+{
+    public class ValidationMessages
+    {
+        public ModelStateDictionary modelState = new ModelStateDictionary();
+
+        public ValidationMessages(Dictionary<string, List<int>> MessageCodes)
+        {
+            foreach(var dict in MessageCodes)
+            {
+                foreach (var code in dict.Value)
+                {
+                    switch (code)
+                    {
+                        case 200:
+                            modelState.AddModelError(dict.Key, "Some error...");
+                            break;
+                        case 201:
+                            modelState.AddModelError(dict.Key, "Some other error...");
+                            break;
+                        case 202:
+                        case 203:
+                            modelState.AddModelError(dict.Key, "many Error with same message");
+                            break;
+                        case 204:
+                            modelState.AddModelError(dict.Key, dict.Key + "is required");
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
