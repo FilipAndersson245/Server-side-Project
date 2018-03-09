c7b28a18dc825ae10d9386809fe4522de36ccd7a
jonatan-flyckt <jonatan.flyckt@hotmail.com>
2018-03-09 10:36:32 +0100

    Restructured code from controller to manager, plus fixed some stuff in book repository

--- ServerSide-Project/Controllers/BookAuthorClassificationController.cs
+++ ServerSide-Project/Controllers/BookAuthorClassificationController.cs
@@ -49,34 +49,15 @@ namespace ServerSide_Project.Controllers
         [SetTempDataModelState]
         public ActionResult EditBook(BookAuthorClassification bac, string[] authorChecklist, int classificationRadio)
         {
-            AuthorManager authorManager = new AuthorManager();
-            ClassificationManager classificationManager = new ClassificationManager();
-            BookManager bookManager = new BookManager();
-            Classification classification = classificationManager.GetClassificationFromID(classificationRadio);
-            List<Author> authorList = new List<Author>();
-            foreach (var aID in authorChecklist)
-            {
-                authorList.Add(authorManager.GetAuthorFromID(Convert.ToInt32(aID)));
-            }
-            Book book = new Book()
-            {
-                Title = bac.Book.Title,
-                ISBN = bac.Book.ISBN,
-                Pages = bac.Book.Pages,
-                PublicationYear = bac.Book.PublicationYear,
-                Publicationinfo = bac.Book.Publicationinfo,
-                SignId = classification.SignId,
-                Classification = classification,
-                Authors = authorList
-            };
             ValidateAndRedirect();
-            var bookTuple = bookManager.EditBook(book);
+            BookManager bookManager = new BookManager();
+            var bookTuple = bookManager.EditBook(bac, authorChecklist, classificationRadio);
             if (bookTuple.Item2.IsValid)
                 return RedirectToAction("ListBookDetails", "Book", new { id = bookTuple.Item1.ISBN });
             else
             {
                 ValidationMessages.ConvertCodeToMsg(ModelState, bookTuple.Item2.ErrorDict);
-                return RedirectToAction("EditBook", "BookAuthorClassification", new { id = book.ISBN });
+                return RedirectToAction("EditBook", "BookAuthorClassification", new { id = bac.Book.ISBN });
             }
         }
     }
