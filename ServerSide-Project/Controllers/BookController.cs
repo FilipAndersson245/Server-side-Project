cde379502c8d1b26e06f8dc4f209099c7dcd013f
FilipAndersson245 <anfi1622@E2404-20-1712>
2018-03-15 14:42:37 +0100

    tryed to start working on widget

--- ServerSide-Project/Controllers/BookController.cs
+++ ServerSide-Project/Controllers/BookController.cs
@@ -1,6 +1,10 @@
 ﻿using ServerSide_Project.Tools;
 using Service.Managers;
+using Service.Models;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace ServerSide_Project.Controllers
 {
@@ -48,11 +52,13 @@ namespace ServerSide_Project.Controllers
             return View("BrowseSearchedBooks", bookManager.SearchBooks(search, page, ITEMS_PER_PAGE, classifications));
         }
 
-        //[HttpGet]
-        //public ActionResult SearchBooks(Search search, int page = 1)
-        //{
-        //    BookManager manager = new BookManager();
-        //    return View("BrowseSearchedBooks", manager.SearchBooks(search, page, ITEMS_PER_PAGE));
-        //}
+        [HttpGet]
+        public string SEARCH(string search)
+        {
+            var a = new BookManager().SearchBooks(new Search() { SearchQuery = search, SelectedClassifications = new List<int>() { 1,2,3,4,5 } }, 1, 20);
+            var jsonSerialiser = new JavaScriptSerializer();
+            var json = jsonSerialiser.Serialize(a);
+            return json;
+        }
     }
 }
